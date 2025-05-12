9febeeefd160aad05ba158bc657e1f0b15844c1c
Agustin Gomez Diaz <agustingd18@gmail.com>
2025-05-12 12:02:42 -0300

    Confirmación inicial indice 4

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use App\Models\Category;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // User::factory(10)->create();
+
+        User::factory()->create([
+            'name' => 'Agustín Gómez Díaz',
+            'email' => 'agustíngd18@gmail.com',
+            'password' => bcrypt('L12e#Dy6$'),
+        ]);
+
+        Category::factory(5)->create();
+    }
+}
